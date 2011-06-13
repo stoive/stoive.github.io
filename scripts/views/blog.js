885239fa3abce6e5302343e3c0ac164961f1ff5d
Steven Thurlow <steven@sthurlow.com>
2011-06-13 16:28:26 +1000

    lazy render for all feeds

--- scripts/views/blog.js
+++ scripts/views/blog.js
@@ -3,32 +3,44 @@ define(["text!views/templates/blog-small.txt", "text!views/templates/feed.txt"],
 	
 	var BlogCollectionView = Backbone.View.extend({
 		tagName: "section",
-		id: "twitter-feed",
-		render:	function() {
-			this.el.innerHTML = Mustache.to_html(
-				feedTemplate,
-				{
-					heading: "blog",
-					items: this.model.toJSON(),
-				},
-				{
-					item: blogTemplate
+		id: "blog-feed",
+		render:	(function() {
+			var existing = [];
+			return function() {
+				if (this.$('h2').length === 0) {
+					this.el.innerHTML = Mustache.to_html(
+						feedTemplate,
+						{ heading: "blog" }
+					);
+
+				}	
+				
+				// detect new items
+				var additional = [], 
+					feed = this.model.toJSON(),
+					curr;
+				while (!_.isEqual(curr = feed.shift(), existing[0])) additional.push(curr);
+				existing = feed;
+				
+				// add new items to the top
+				for (var i = additional.length - 1; i >=  0; --i) {
+					var em = document.createElement('article');
+					em.innerHTML = Mustache.to_html(
+						blogTemplate,
+						additional[i]
+					);
+					this.$('h2').after(em);
 				}
-			);
+				
+				// remove items which drop off the bottom of the screen
+				while ($(this.el).children().length > 2 && $(document).height() > $(window).height())
+				{
+					$(this.el).children().last().remove()
+				}
 
-			setTimeout(function() {
-				this.$('p').each(function(i, e) {
-					e.innerHTML.replace(/[\d%]+/g, "<span class='digits'>$&</span>");
-				});
-			}, 0);
-
-			while ($(this.el).children().length > 2 && $(document).height() > $(window).height())
-			{
-				$(this.el).children().last().remove()
+				return this;
 			}
-
-			return this;
-		}
+		})()
 	});
 	
 	return BlogCollectionView;
--- scripts/views/checkin.js
+++ scripts/views/checkin.js
@@ -2,26 +2,44 @@ define(["text!views/templates/checkin-small.txt", "text!views/templates/feed.txt
 
 	var CheckinCollectionView = Backbone.View.extend({
 		tagName: "section",
-		id: "twitter-feed",
-		render:	function() {
-			this.el.innerHTML = Mustache.to_html(
-				feedTemplate,
-				{
-					heading: "github",
-					items: this.model.toJSON(),
-				},
-				{
-					item: checkinTemplate
+		id: "github-feed",
+		render:	(function() {
+			var existing = [];
+			return function() {
+				if (this.$('h2').length === 0) {
+					this.el.innerHTML = Mustache.to_html(
+						feedTemplate,
+						{ heading: "github" }
+					);
+
+				}	
+				
+				// detect new items
+				var additional = [], 
+					feed = this.model.toJSON(),
+					curr;
+				while (!_.isEqual(curr = feed.shift(), existing[0])) additional.push(curr);
+				existing = feed;
+				
+				// add new items to the top
+				for (var i = additional.length - 1; i >=  0; --i) {
+					var em = document.createElement('article');
+					em.innerHTML = Mustache.to_html(
+						feedTemplate,
+						additional[i]
+					);
+					this.$('h2').after(em);
 				}
-			);
-			
-			while ($(this.el).children().length > 2 && $(document).height() > $(window).height())
-			{
-				$(this.el).children().last().remove()
+				
+				// remove items which drop off the bottom of the screen
+				while ($(this.el).children().length > 2 && $(document).height() > $(window).height())
+				{
+					$(this.el).children().last().remove()
+				}
+
+				return this;
 			}
-			
-			return this;
-		}
+		})()
 	});
 	return CheckinCollectionView;
 });
