64b21d5e4aa3534b82c6fa83e37bd6ca06beb2aa
Steven Thurlow <steven@sthurlow.com>
2012-08-18 13:42:06 +0200

    protect email

---
+++ resume/main.js
@@ -0,0 +1,9 @@
+require(['jquery-1.8.0.min', 'underscore-min'], function() {
+	$('button.get-email').click(function() {
+		var a = document.createElement('a');
+		a.href = "m1a2i3l4t5o6:7s8t9e0v1e2n3@4s5t6h7u8r9l0o1w2.3c4o5m6";
+		a.href = a.href.replace(/\d/g, '');
+		$(a).text(a.href.replace('mailto:', ''));
+		$(this).replaceWith(a);
+	});
+});
